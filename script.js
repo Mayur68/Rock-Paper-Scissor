db7bae12010d4e64ed7fff25b87fdbd42583a378
mayur <89019990+Mayur68@users.noreply.github.com>
2022-07-24 12:43:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 let s1 = document.getElementById("sc-bsx");
-var s2 = document.getElementById("sc-bsx1");
+let s2 = document.getElementById("sc-bsx1");
 let s3 = document.querySelector("sc-bsx2");
+
+var rock=document.getElementById("rock");
+var paper=document.getElementById("paper");
+var scissor=document.getElementById("scissor");
+var rock1=document.getElementById("rock1");
+var paper1=document.getElementById("paper1");
+var scissor1=document.getElementById("scissor1");
 
 let y = 1;
 let z = 0;
@@ -49,22 +56,76 @@ function computer_turn(com) {
 
 function wincheck(cr, pr) {
     if (cr == 1 && pr == 1) {
+       rock.style.color="white";
+        rock1.style.color="white";
+        paper.style.color="";
+        paper1.style.color="";
+        scissor.style.color="";
+        scissor1.style.color="";
         return a;
     } else if (cr == 2 && pr == 2) {
+        rock.style.color="";
+        rock1.style.color="";
+        paper.style.color="white";
+        paper1.style.color="white";
+        scissor.style.color="";
+        scissor1.style.color="";
         return a;
     } else if (cr == 3 && pr == 3) {
+        rock.style.color="";
+        rock1.style.color="";
+        paper.style.color="";
+        paper1.style.color="";
+        scissor.style.color="white";
+        scissor1.style.color="white";
         return a;
     } else if (cr == 1 && pr == 2) {
+        rock.style.color="";
+        rock1.style.color="red";
+        paper.style.color="white";
+        paper1.style.color="";
+        scissor.style.color="";
+        scissor1.style.color="";
         return b;
     } else if (cr == 1 && pr == 3) {
+        rock.style.color="";
+        rock1.style.color="white";
+        paper.style.color="";
+        paper1.style.color="";
+        scissor.style.color="red";
+        scissor1.style.color="";
         return c;
     } else if (cr == 2 && pr == 1) {
+        rock1.style.color="";
+        paper.style.color="";
+        paper1.style.color="white";
+        rock.style.color="red";
+        scissor.style.color="";
+        scissor1.style.color="";
         return c;
     } else if (cr == 2 && pr == 3) {
+        rock.style.color="";
+        rock1.style.color="";
+        paper.style.color="";
+        paper1.style.color="red";
+        scissor.style.color="white";
+        scissor1.style.color="";
         return b;
     } else if (cr == 3 && pr == 1) {
+        rock1.style.color="";
+        paper.style.color="";
+        paper1.style.color="";
+        scissor.style.color="";
+        scissor1.style.color="red";
+        rock.style.color="white";
         return b;
     } else if (cr == 3 && pr == 2) {
+        rock.style.color="";
+        rock1.style.color="";
+        paper1.style.color="";
+        scissor.style.color="";
+        scissor1.style.color="red";
+        paper.style.color="white";
         return c;
     }
 
